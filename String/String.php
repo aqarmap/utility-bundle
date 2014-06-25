274f7c6ea29e5910d614c5bfd8494159f0f1e381
Khaled Attia <khaled.attia@googlemail.com>
2014-06-26 02:18:16 +0300

    Added method to create comma separated list from array

---
+++ String/String.php
@@ -0,0 +1,15 @@
+<?php
+namespace Aqarmap\Bundle\UtilityBundle\String;
+
+class String
+{
+    public static function toList($list, $and = 'and', $separator = ', ')
+    {
+        if( count($list) > 1 ) {
+            return implode($separator, array_slice($list, NULL, -1)) .' '. $and .' '. array_pop($list);
+        }
+        else {
+            return array_pop($list);
+        }
+    }
+}
